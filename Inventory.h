0afed09bbbf5371de01664cb2db7a82f441d7210
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2022-04-24 01:40:59 +0300

    Added class Inventory

---
+++ Inventory.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <map>
+#include <string>
+
+using namespace std;
+
+class Inventory
+{
+public:
+	static void Change_Item(string item_name, int number);
+	static int Show_Item(string item_name);
+	static bool Check_Tool(string tool_id);
+private:
+	static int food, water, wood, tinder, medicine;
+	map<string, bool> tools;
+};
